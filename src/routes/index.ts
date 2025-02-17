ff9d70f77667a6007c7a5fe5311af83277b20d7b
zeroboycoder <pyaesonekhant@tumawlamyine.edu.mm>
2025-02-17 20:20:04 +0700

    Add health check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,4 +7,11 @@ const route = Router()
 route.use('/admin', adminRoutes)
 route.use('/user', userRoutes)
 
+route.use("/health-check", (req: any, res: any) => {
+  return res.status(200).json({
+    status: "success",
+    message: "API is working"
+  })
+})
+
 export default route;
