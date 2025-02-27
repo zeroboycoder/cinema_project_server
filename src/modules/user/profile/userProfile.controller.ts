cc76f20d397a1fe4c2e0eb6d755220702bdac0ce
zeroboycoder <pyaesonekhant@tumawlamyine.edu.mm>
2025-02-27 14:43:05 +0700

    Fix in user password

--- src/modules/user/profile/userProfile.controller.ts
+++ src/modules/user/profile/userProfile.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import UserModel from '@models/user.model'
+import bcrypt from 'bcrypt'
 import { p } from '@utils/index'
 import { successResponse } from '@utils/response'
 
@@ -24,7 +25,7 @@ export const getUserProfileById = async (req: Request, res: Response, next: Next
 
 export const updateUserProfile = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const params: { userId?: number, name?: string, email?: string } = p(req)
+    const params: { userId?: number, name?: string, email?: string, oldPassword?: string, newPassword?: string } = p(req)
 
     if (!params.userId) throw new Error("User id is required");
 
@@ -37,6 +38,25 @@ export const updateUserProfile = async (req: Request, res: Response, next: NextF
     if (!user) {
       throw new Error("User not found")
     }
+
+    if (params.oldPassword && params.newPassword) {
+      const validPassword = await bcrypt.compare(params.oldPassword, user.password)
+      if (!validPassword) {
+        throw new Error('Invalid credentials')
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(params.newPassword, salt);
+
+      await UserModel.update({
+        password: hashedPassword
+      }, {
+        where: {
+          id: user.id
+        }
+      })
+    }
+
 
     await UserModel.update({
       name: params.name,
